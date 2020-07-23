26f92e25de33bf6dfd38ee20f4d627efa10cde3a
Raphael Kuttruf <Raphael.Kuttruf@gmx.net>
2020-07-23 22:59:14 +0200

    Empty console app VS solution

---
+++ ExploringDynamicLibraries/ExploringDynamicLibraries/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExploringDynamicLibraries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
