646feb26cd074721d90aea6d2bb781e2e1474f45
Raphael Kuttruf <Raphael.Kuttruf@gmx.net>
2020-07-23 23:16:48 +0200

    Implemented library interface for class HellowWorld

--- ExploringDynamicLibraries/MyFirstLibrary/HelloWorld.cs
+++ ExploringDynamicLibraries/MyFirstLibrary/HelloWorld.cs
@@ -2,19 +2,41 @@
 
 namespace MyFirstLibrary
 {
-    public class HelloWorld
+    public class HelloWorld : IMyFirstLibrary
     {
         /// <summary>
         /// Additional message to the world
         /// </summary>
         public string Message { get; set; }
-        
+
         /// <summary>
         /// Default Construktor
         /// </summary>
         public HelloWorld()
         {
         }
+
+        public void Abort()
+        {
+            Console.WriteLine($"Aborting {typeof(HelloWorld).FullName}... :(");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"Disposing {typeof(HelloWorld).FullName}.");
+        }
+
+        public void Initialize()
+        {
+            Console.WriteLine($"Initialiszing {typeof(HelloWorld).FullName}. :)");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"Running {typeof(HelloWorld).FullName}. :D");
+            SayHelloWorld();
+        }
+
         /// <summary>
         /// Write "Hello World :)" and additional Message to console
         /// </summary>
