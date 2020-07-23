4d166f2157888b3719824737dc23ddceb9d3c58c
Raphael Kuttruf <Raphael.Kuttruf@gmx.net>
2020-07-23 23:13:06 +0200

    Implemented simple Interface for all classes in the library

---
+++ ExploringDynamicLibraries/MyFirstLibrary/IMyFirstLibrary.cs
@@ -0,0 +1,13 @@
+﻿namespace MyFirstLibrary
+{
+    /// <summary>
+    /// Every class inside this Library implements this interface.
+    /// </summary>
+    public interface IMyFirstLibrary
+    {
+        void Initialize();
+        void Run();
+        void Abort();
+        void Dispose();
+    }
+}
