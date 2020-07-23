135f49b9a5e1bbca8e20ad8375905fcfa7f03890
Raphael Kuttruf <Raphael.Kuttruf@gmx.net>
2020-07-23 23:09:26 +0200

    Implemented class HelloWorld in the library project

---
+++ ExploringDynamicLibraries/MyFirstLibrary/HelloWorld.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyFirstLibrary
+{
+    public class HelloWorld
+    {
+        /// <summary>
+        /// Additional message to the world
+        /// </summary>
+        public string Message { get; set; }
+        
+        /// <summary>
+        /// Default Construktor
+        /// </summary>
+        public HelloWorld()
+        {
+        }
+        /// <summary>
+        /// Write "Hello World :)" and additional Message to console
+        /// </summary>
+        private void SayHelloWorld()
+        {
+            Console.WriteLine("Hello World :)");
+            if (Message.Length > 0)
+            {
+                Console.WriteLine(Message);
+            }
+        }
+    }
+}
